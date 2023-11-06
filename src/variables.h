c24080849d76b97a2bc9a92ee8f0a802f4bb6a2e
RagavN12 <96319441+RagavN12@users.noreply.github.com>
2023-11-05 19:13:02 -0500

    forgot to save

---
+++ src/variables.h
@@ -0,0 +1,49 @@
+/*
+This file contains the important constants needed for our Robot.
+Please add or change any constants that may be nessesary.
+Please add the PORT numbers.
+
+*/
+
+
+// ------- PORT NUMBERS ---------
+
+#ifndef VARIABLES
+#define VARIABLES
+
+// Left Drive Motors
+
+#define LEFT_MOTOR_FRONT
+#define LEFT_MOTOR_BACK
+
+// Right Drive Motors
+
+#define RIGHT_MOTOR_FRONT
+#define RIGHT_MOTOR_BACK
+
+// Intake Motors
+
+#define INTAKE_MOTOR
+
+// Turret Motors
+
+#define Turret_Motor
+
+// Puncher Motor for catapult
+
+#define Puncher_Motor
+
+// Wings
+
+#define PTO_MOTOR_LEFT
+#define PTO_MOTOR_RIGHT
+
+
+// ------- EXTRA CONSTANTS -------------- 
+
+// Motor Speeds 
+
+#define INTAKE_MOTOR_SPEED
+#define Turret_Motor_SPEED
+
+#endif // VARIABLES
